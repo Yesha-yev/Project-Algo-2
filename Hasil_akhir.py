9ad7b6862314fcceb7154ee279b9d0d35c4b490a
Yesha-yev <242410103084@mail.unej.ac.id>
2025-05-30 13:49:42 +0700

    perubahan baru

--- Hasil_akhir.py
+++ Hasil_akhir.py
@@ -3,16 +3,24 @@ import Login as log
 from tabulate import tabulate as tb
 
 def dashboard(pengguna):
+    print('''
+    Halo Selamat Datang!!
+    saat ini ada ..... jumlah lahan''')
     print(
 """
 ╔══════════════════════════════╗
 ║        Dashboard Pengguna    ║
 ║┌────────────────────────────┐║
-║│  1. Perhitungan Kebutuhan  │║
-║│     Bibit                  │║
-║│  2. History Panen          │║
-║│  3. Manajemen Data Tanaman │║
-║│  4. Manajemen Lahan        │║
+║│  1. Input Data Lahan &     │║
+║│     Tanaman                │║
+║│  2. Integrasi Harga Pupuk  │║
+║│  3. Kalkulasi Kebutuhan    │║
+║│     Nutrisi                │║
+║│  3. Simulasi Biaya Pupuk   │║
+║│     Hemat                  │║
+║│  4. Hitung Kebutuhan Bibit │║
+║│     setiap Lahan           │║
+║│  4. Tampilan Rekomendasi   │║
 ║├────────────────────────────┤║
 ║│  5. Keluar                 │║
 ║└────────────────────────────┘║
